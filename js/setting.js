b8daeaf81a62621d7d41b4fd05e1151c624978f4
shenmi124 <67265011@qq.com>
2022-08-13 11:50:21 +0800

    焯

--- js/setting.js
+++
@@ -1,9 +0,0 @@
-function autoSave(){
-	if(player.autoSave=="false"){
-		player.autoSave = "true"
-		player.saveTick = 'false'
-	}else{
-		player.autoSave = "false"
-		player.saveTick = 'false'
-	}
-}
